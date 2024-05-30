ebf026169bb12d42bfaf8637a5c4bda8190c8257
Mrskpatel <kasundrasarthi72@gmail.com>
2024-05-30 18:07:40 +0530

    PR-10

---
+++ PR-10/pr-10/src/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li><a href="#about">About</a></li>
+        <li><a href="#projects">Projects</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
